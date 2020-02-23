9017971c7a653ab3686cf4522cea17a21d2dfe31
Ataberk Özen <ataberkozen123@gmail.com>
2020-02-23 06:59:14 +0300

    j6primelte: inherit from msm8917-common
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# Device Tree for Samsung Galaxy J6+ (Plus)
+
+## Spec Sheet
+
+| Feature                 | Specification                     |
+| :---------------------- | :-------------------------------- |
+| CPU                     | Quad-core 1.4 GHz Cortex-A53      |
+| Chipset                 | Qualcomm MSM8917 Snapdragon 425   |
+| GPU                     | Adreno 308                        |
+| Memory                  | 3/4 GB	                          |
+| Shipped Android Version | 8.1.0                             |
+| Storage                 | 32/64 GB                          |
+| MicroSD                 | Up to 512 GB                      |
+| Battery                 | 3300 mAh (non-removable)          |
+| Dimensions              | 161.4 x 76.9 x 7.9 mm             |
+| Display                 | 1480x720 pixels, 6.0 (~274 PPI)   |
+| Rear Camera             | 13 MP, 5 MP, LED flash            |
+| Front Camera            | 8 MP                              |
+| Release Date            | October 2018                      |
+
+## Device Picture
+
+![Galaxy J6+](https://s2.turkcell.com.tr//SiteAssets/Cihaz/cep-telefonu/samsung/galaxy-j6-plus/cg/7.png "Galaxy J6+")
---
+++ device.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2019-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from msm8917-common
+$(call inherit-product, device/samsung/msm8917-common/msm8917.mk)
+
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/sdm450_camera_j6primelte.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/sdm450_camera_j6primelte.xml
+
+# Fingerprint
+#PRODUCT_PACKAGES += \
+#    fingerprint.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.samsung
+	
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    device/samsung/j6primelte
+
+# Inherit vendor
+$(call inherit-product, vendor/samsung/j6primelte/j6primelte-vendor.mk)
